99ea4b91dafc46416e12b1bbdfd49ace2fd31cde
Manabu Niseki <manabu.niseki@gmail.com>
2020-06-28 19:28:43 +0900

    refactor: improve input validation
    Add mime-type based input validation

--- tests/api/endpoints/test_analyze.py
+++ tests/api/endpoints/test_analyze.py
@@ -4,8 +4,8 @@ from tests.conftest import read_file
 
 
 @pytest.mark.asyncio
-async def test_analyze(client, emailrep_response):
-    payload = {"eml_file": read_file("sample.eml")}
+async def test_analyze(client):
+    payload = {"file": read_file("sample.eml")}
     response = await client.post("/api/analyze/", json=payload)
 
     json = response.json()
@@ -14,10 +14,26 @@ async def test_analyze(client, emailrep_response):
 
 
 @pytest.mark.asyncio
-async def test_analyze_file(client, emailrep_response):
+async def test_analyze_with_invalid_file(client):
+    payload = {"file": ""}
+    response = await client.post("/api/analyze/", json=payload)
+
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_analyze_file(client):
     data = {"file": read_file("sample.eml").encode()}
     response = await client.post("/api/analyze/file", data=data)
 
     json = response.json()
     assert json.get("eml", {}).get("header", {}).get("subject") == "Winter promotions"
     assert json.get("eml", {}).get("header", {}).get("from") == "no-reply@example.com"
+
+
+@pytest.mark.asyncio
+async def test_analyze_file_with_invalid_file(client):
+    data = {"file": b""}
+    response = await client.post("/api/analyze/file", data=data)
+
+    assert response.status_code == 422
