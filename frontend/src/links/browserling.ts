8c6d61950265e794c80a57c9954a8089407156a8
Manabu Niseki <manabu.niseki@gmail.com>
2020-08-04 20:53:33 +0900

    feat: add browserling link
    Implement #22

---
+++ frontend/src/links/browserling.ts
@@ -0,0 +1,23 @@
+import { Link, LinkType } from "@/types";
+import { buildURL } from "@/utils/url_builder";
+
+export class Browserling implements Link {
+  public baseURL: string;
+  public favicon: string;
+  public name: string;
+  public type: LinkType;
+
+  public constructor() {
+    this.baseURL = "https://www.browserling.com";
+    this.favicon = "https://www.google.com/s2/favicons?domain=browserling.com";
+    this.name = "Browserling";
+    this.type = "url";
+  }
+
+  public href(value: string): string {
+    return buildURL(
+      this.baseURL,
+      `/browse/win/7/ie/11/${encodeURIComponent(value)}`
+    );
+  }
+}
---
+++ frontend/tests/unit/links/browserling.spec.ts
@@ -0,0 +1,20 @@
+import { Browserling } from "@/links/browserling";
+
+describe("Browserling", function () {
+  const subject = new Browserling();
+
+  describe("#type", function () {
+    it("equals to url", function () {
+      expect(subject.type).toEqual("url");
+    });
+  });
+
+  describe("#href", function () {
+    it("returns URL", function () {
+      const hash = "http://example.com";
+      expect(subject.href(hash)).toEqual(
+        "https://www.browserling.com/browse/win/7/ie/11/http%3A%2F%2Fexample.com"
+      );
+    });
+  });
+});
