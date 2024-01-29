7f7ea3f5e7369caf9c50cd4ff878886a22eef77a
Michał Praszmo <michalpr@cert.pl>
2024-01-29 11:57:31 +0100

    Better import

--- backend/api/endpoints/lookup.py
+++ backend/api/endpoints/lookup.py
@@ -4,7 +4,7 @@ from redis import StrictRedis
 from fastapi import APIRouter, HTTPException
 
 from backend.schemas.response import Response
-import backend.core.settings
+from backend.core.settings import REDIS_HOST, REDIS_PASSWORD
 
 router = APIRouter()
 
@@ -16,10 +16,10 @@ router = APIRouter()
     status_code=200,
 )
 async def lookup(identifier: str) -> Response:
-    if not backend.core.settings.REDIS_HOST:
+    if not REDIS_HOST:
         raise HTTPException(502)
 
-    redis_conn = StrictRedis(host=backend.core.settings.REDIS_HOST, password=backend.core.settings.REDIS_PASSWORD)
+    redis_conn = StrictRedis(host=REDIS_HOST, password=REDIS_PASSWORD)
     data = redis_conn.hget("results", identifier)
     if not data:
         raise HTTPException(404)
