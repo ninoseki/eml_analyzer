b4c906e0a7514dd5c31548f919fbfe7e7358baaf
Michał Praszmo <michalpr@cert.pl>
2024-01-29 13:09:10 +0100

    Make redis opt-in

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,6 +24,6 @@ VIRUSTOTAL_API_KEY: Secret = config("VIRUSTOTAL_API_KEY", cast=Secret, default="
 
 INQUEST_API_KEY: Secret = config("INQUEST_API_KEY", cast=Secret, default="")
 
-REDIS_HOST : str = config("REDIS_HOST", cast=str, default="redis")
+REDIS_HOST : str = config("REDIS_HOST", cast=str, default="")
 REDIS_PASSWORD: str = config("REDIS_PASSWORD", cast=str, default="changeme")
 REDIS_EXPIRE: str = config("REDIS_EXPIRE", cast=int, default=3600)
