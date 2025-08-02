e12386f19991ac8732eee6ec44c9a0e2901b711f
Manabu Niseki <manabu.niseki@gmail.com>
2025-08-02 09:14:29 +0900

    fix: fix vitest config error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const target = env.VITE_BACKEND_URL || 'http://localhost:8000'
   return {
-    plugins: [tailwindcss(), vue()],
+    plugins: [tailwindcss(), vue(), vueDevTools()],
     server: {
       proxy: {
         '/api/': target
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,17 +1,18 @@
 import { fileURLToPath } from 'node:url'
 
-import { UserConfig } from 'vite'
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig as UserConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
+export default defineConfig((config) =>
+  mergeConfig(
+    viteConfig(config),
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url))
+      }
+    })
+  )
 )
