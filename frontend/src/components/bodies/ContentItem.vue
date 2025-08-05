0967707208fde83d6f33deecafa2d94ecb94ec8e
Manabu Niseki <manabu.niseki@gmail.com>
2025-08-05 15:39:10 +0900

    feat: show HTML in iframe sandbox (#261)

--- frontend/src/components/bodies/ContentItem.vue
+++ frontend/src/components/bodies/ContentItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computedAsync } from '@vueuse/core'
 import { codeToHtml } from 'shiki'
+import { computed } from 'vue'
 
 const props = defineProps({
   content: {
@@ -12,19 +13,35 @@ const props = defineProps({
   }
 })
 
-const html = computedAsync(async () => {
+const isHTMLContent = computed(() => {
+  return (props.contentType || '').startsWith('text/html')
+})
+
+const code = computedAsync(async () => {
   const render = async () => {
-    if (props.contentType === 'text/html') {
+    if (isHTMLContent.value) {
       return await codeToHtml(props.content, { lang: 'html', theme: 'github-dark-default' })
     }
     return await codeToHtml(props.content, { lang: 'text', theme: 'github-dark-default' })
   }
   return await render()
 })
+
+const gridColumns = computed(() => {
+  return isHTMLContent.value ? 'grid-cols-2' : 'grid-cols-1'
+})
 </script>
 
 <template>
-  <div class="mockup-code w-full px-6 my-4 bg-[#0d1117]" v-html="html"></div>
+  <div :class="['grid', gridColumns]">
+    <div class="mockup-code w-full h-96 overflow-scroll px-6 my-4 bg-[#0d1117]" v-html="code"></div>
+    <iframe
+      class="w-full h-96 border-0 px-6 my-4"
+      :srcdoc="content"
+      sandbox=""
+      v-if="isHTMLContent"
+    ></iframe>
+  </div>
 </template>
 
 <style>
@@ -32,10 +49,6 @@ pre.shiki > code {
   white-space: pre-wrap;
   word-break: break-all;
   word-wrap: break-word;
-}
-
-pre.shiki {
-  overflow-x: auto;
 }
 
 pre.shiki::before {
