2d34518bf721b9ed1fac1427cca5acb6528ad1b4
Manabu Niseki <manabu.niseki@gmail.com>
2025-09-05 19:34:18 +0900

    fix: mitigate magic issue

--- tests/factories/test_eml.py
+++ tests/factories/test_eml.py
@@ -1,3 +1,6 @@
+import glob
+from pathlib import Path
+
 import pytest
 
 from backend import factories
@@ -64,10 +67,17 @@ def test_content_id(content_id_eml: bytes, factory: factories.EmlFactory):
     assert first.content_id == "<id42@guppylake.bellcore.com>"
 
 
-def test_emails(emails: list[bytes], factory: factories.EmlFactory):
-    for email in emails:
-        eml = factory.call(email)
-        assert eml is not None
+def get_email_paths():
+    return [Path(p) for p in glob.glob("tests/fixtures/emails/**/*.eml")]
+
+
+@pytest.fixture(params=get_email_paths())
+def email_path(request: pytest.FixtureRequest):
+    return request.param
+
+
+def test_email(email_path: Path, factory: factories.EmlFactory):
+    assert factory.call(email_path.read_bytes()) is not None
 
 
 def test_complete_msg(complete_msg: bytes, factory: factories.EmlFactory):
