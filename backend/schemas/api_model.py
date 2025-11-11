67f9c526c8728931ac104629735bcdc21ed012a7
Manabu Niseki <manabu.niseki@gmail.com>
2025-11-11 16:40:54 +0900

    refactor: remove pyhumps (#283)

--- backend/schemas/api_model.py
+++ backend/schemas/api_model.py
@@ -1,8 +1,8 @@
-from humps import camelize
 from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class APIModel(BaseModel):
     model_config = ConfigDict(
-        from_attributes=True, alias_generator=camelize, populate_by_name=True
+        from_attributes=True, alias_generator=to_camel, populate_by_name=True
     )
