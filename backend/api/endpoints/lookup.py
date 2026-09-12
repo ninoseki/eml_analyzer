6407619d056b83c61626bf9178f2ea7dcc4f228b
Manabu Niseki <manabu.niseki@gmail.com>
2026-09-12 12:09:06 +0900

    chore: update actions & backend dependencies

--- backend/api/endpoints/lookup.py
+++ backend/api/endpoints/lookup.py
@@ -12,11 +12,11 @@ router = APIRouter()
     description="Try to fetch existing analysis from database",
 )
 async def lookup(id: str, *, redis: dependencies.RequiredRedis) -> schemas.Response:
-    got: bytes | None = await redis.get(f"{settings.REDIS_KEY_PREFIX}:{id}")
+    got = await redis.get(f"{settings.REDIS_KEY_PREFIX}:{id}")
     if got is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Cache not found",
         )
 
-    return schemas.Response.model_validate_json(got.decode())
+    return schemas.Response.model_validate_json(got)
